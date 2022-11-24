1b1b56dd416f32d76e74683ff418e79f2fc5b8da
vvrvishal <vishalguptaramasare@gmail.com>
2022-11-25 00:42:57 +0530

    changes on portfolip

--- src/components/FeedBack.js
+++ src/components/FeedBack.js
@@ -11,49 +11,39 @@ export default function FeedBack(props) {
         {
             fb: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected',
             clientName: 'Abhishek Gupta'
-        },]
+        }];
 
     const feedbackHtml = feedbackArray.map((value, index) => {
-        return <div className="item">
-            <div className="align-items-center row border">
-                <div className="col-md-5">
-                    <img src={props.image} className='w-100' alt=""/>
-                </div>
-                <div className="col-md-7">
-                    <div className="">
-                        <img src={props.quoteIcon} className='mb-4 pb-1' style={{width: '4rem'}} alt=""/>
-                        <h3 className='playfairFont'
-                            style={{lineHeight: '1.3', wordSpacing: '3px', letterSpacing: '1px'}}>
-                            {value.fb}</h3>
-                        <h6 className='mt-4'>- {value.clientName}</h6>
-                        {/*<img src={props.feedbackImage} style={{bottom: '12%', right: '10%'}}*/}
-                        {/*     className='position-absolute' alt=""/>*/}
+        return <div className='item' key={index}>
+                    <div className="align-items-center row border">
+                        <div className="col-md-5">
+                            <img src={props.image} className='w-100' alt=""/>
+                        </div>
+                        <div className="col-md-7">
+                            <div className="">
+                                <img src={props.quoteIcon} className='mb-4 pb-1' style={{width: '4rem'}} alt=""/>
+                                <h3 className='playfairFont'
+                                    style={{lineHeight: '1.3', wordSpacing: '3px', letterSpacing: '1px'}}>
+                                    {value.fb}</h3>
+                                <h6 className='mt-4'>- {value.clientName}</h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
+    });
 
-            </div>
-        </div>
-    })
-    const options = {
-        responsive: {
-            0: {
-                items: 1
-            }
-        }
-    }
     return (
         <>
             <div className="align-items-center container-md-fluid d-flex darknavyBg position-relative text-white"
                  style={{minHeight: '100vh'}}>
                 <div className="justify-content-around row mx-0">
                     <div className="col-md-11">
-                        <OwlCarousel nav='false' dots='false' autoplay='false' loop
-                                     className='FeedbackCarousel' {...options}>
+                        <OwlCarousel nav='false' dots='false' autoplay='true' loop className='sk'>
                             {feedbackHtml}
-                        </OwlCarousel>;
+                        </OwlCarousel>
                     </div>
                 </div>
             </div>
         </>
     )
-}
+}   
